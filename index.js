629405beb63fb624658d64fe5409c778f0ed7e74
Anyang Diing <anyang.ngor@student.moringaschool.com>
2024-12-26 19:16:23 +0300

    First Class Functions

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+
+// Function that receives a callback and calls it
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  // Function that returns a named function
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      console.log("This is a named function.");
+    };
+  }
+  
+  // Function that returns an anonymous function
+  function returnsAnAnonymousFunction() {
+    return function () {
+      console.log("This is an anonymous function.");
+    };
+  }
+  
+  module.exports = { receivesAFunction, returnsANamedFunction, returnsAnAnonymousFunction };
+  
